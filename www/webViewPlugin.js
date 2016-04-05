705a54ba288640087e3da7f74bd6b538ef480d7c
sebalagos90 <sebalegutie@gmail.com>
2016-04-05 15:54:42 -0300

    Including loading when blank page is shown. Adding HideLoading function to close the loading in ionic afterEnter event.

--- www/webViewPlugin.js
+++ www/webViewPlugin.js
@@ -3,12 +3,21 @@
 module.exports = (function() {
 
 
-  var _show = function(url, successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, 'WebViewPlugin', 'show', [url]);
+  var _show = function(url, successCallback, errorCallback, loading) {
+    if(loading){
+      cordova.exec(successCallback, errorCallback, 'WebViewPlugin', 'show', [url, loading]);
+    }
+    else{
+      cordova.exec(successCallback, errorCallback, 'WebViewPlugin', 'show', [url]);
+    }
   };
 
   var _hide = function(successCallback, errorCallback) {
     cordova.exec(successCallback, errorCallback, 'WebViewPlugin', 'hide', []);
+  };
+
+  var _hideLoading = function(successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, 'WebViewPlugin', 'hideLoading', []);
   };
 
   var _subscribeCallback = function(successCallback, errorCallback) {
@@ -30,7 +39,8 @@ module.exports = (function() {
     Close: _hide,
     SubscribeCallback: _subscribeCallback,
     SubscribeExitCallback: _subscribeExitCallback,
-    ExitApp: _exitApp
+    ExitApp: _exitApp,
+    HideLoading: _hideLoading
   };
 
 })();
