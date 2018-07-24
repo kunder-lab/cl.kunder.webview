bc9b239433badf584d47c79fd68232d39a853bc6
David Govea <govea.d@gmail.com>
2018-07-24 15:35:46 -0700

    restore url-prefix regex to v2.6.0 functionality
    this was changed in commit d8a3edf8

--- src/android/cl/kunder/webview/WebViewActivity.java
+++ src/android/cl/kunder/webview/WebViewActivity.java
@@ -35,7 +35,7 @@ public class WebViewActivity extends CordovaActivity {
             showLoading();
         }
         
-        loadUrl((url.matches("^(.://|javascript:)[\\s\\S]$") ? "" : "file:///android_asset/www/" + (isPluginCryptFileActive() ? "+++/" : "")) + url);
+        loadUrl((url.matches("^(.*://|javascript:)[\\s\\S]*$") ? "" : "file:///android_asset/www/" + (isPluginCryptFileActive() ? "+++/" : "")) + url);
     }
 
     public static boolean showLoading() {
